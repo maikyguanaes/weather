9e15f5045035a05d375ba9d11e3e8abf5448d5a7
Maiky Guanaes <contato@maiky.dev>
2024-08-05 00:16:25 -0300

    test(tests) makes tests improvements

--- tests/Feature/Models/CityTest.php
+++ tests/Feature/Models/CityTest.php
@@ -3,16 +3,21 @@
 use App\Models\City;
 
 it('creates city', function () {
+   # given
    $city = City::factory()->create();
  
+   # then
    expect($city)->toBeInstanceOf(City::class);
 });
 
 it('gets city by alias', function () {
+   # given
     $city = City::factory()->as_sp()->create();
 
+    # when
     $fetched_city = City::byAlias('சாவோ பாவுலோ')->first();
   
+    # then
     expect($fetched_city->id)->toBe($city->id);
  });
  
